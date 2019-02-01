a088a610ccb18448d3c2a827d89cacd89eafc6b1
skaipio <senni.kaipio@gmail.com>
2019-02-01 09:41:00 +0200

    Tehtävä 2.4. Kurssitiedot step9

--- src/App.js
+++ src/App.js
@@ -2,35 +2,51 @@ import React from 'react'
 import Course from './components/Course'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack -sovelluskehitys',
-    parts: [
-      {
-        name: 'Reactin perusteet',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Tiedonvälitys propseilla',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'Komponenttien tila',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Craft Beer',
-        exercises: 420,
-        id: 4
-      },
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack -sovelluskehitys',
+      id: 1,
+      parts: [
+        {
+          name: 'Reactin perusteet',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Tiedonvälitys propseilla',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'Komponenttien tila',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 2,
+          id: 1
+        },
+        {
+          name: 'Middlewaret',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  const courseRows = () => courses.map((course) => <Course key={course.id} course={course} />)
 
   return (
     <div>
-      <Course course={course} />
+      {courseRows()}
     </div>
   )
 }
